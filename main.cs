529b78e980d69ca99607975d8f96f3e90c8cfe63
vorozheikinaanastasia1 <154608865+vorozheikinaanastasia1@users.noreply.github.com>
2023-12-22 17:25:05 +0300

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,67 @@
+/******************************************************************************
+
+                            Online C# Compiler.
+                Code, Compile, Run and Debug C# program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+using System;
+class HelloWorld {
+  static void Main() {
+    Console.WriteLine("Hello World");
+  }using System;
+
+namespace RandomArraySum
+{
+class Program
+{
+static void Main(string[] args)
+{
+Console.WriteLine("Введите длину массива:");
+int length = Convert.ToInt32(Console.ReadLine());
+
+int[] array = GenerateRandomArray(length);
+Console.WriteLine("Сгенерированный массив:");
+PrintArray(array);
+
+int sumOfEvenNumbers = CalculateSumOfEvenNumbers(array);
+Console.WriteLine($"Сумма четных чисел: {sumOfEvenNumbers}");
+}
+
+static int[] GenerateRandomArray(int length)
+{
+int[] array = new int[length];
+Random random = new Random();
+for (int i = 0; i < length; i++)
+{
+array[i] = random.Next(1, 101); // Генерация случайного числа от 1 до 100
+}
+return array;
+}
+
+static void PrintArray(int[] array)
+{
+foreach (int number in array)
+{
+Console.Write(number + " ");
+}
+Console.WriteLine();
+}
+
+static int CalculateSumOfEvenNumbers(int[] array)
+{
+int sum = 0;
+foreach (int number in array)
+{
+if (number % 2 == 0) // Проверка на четность
+{
+sum += number;
+}
+}
+return sum;
+}
+}
+} 
+
+}
